1f040a016dbc73aaa388974bc9784b200db907f5
sengjun0624 <sengjun0624@naver.com>
2025-01-20 11:23:01 +0900

    feat: week4 동적 렌더링 await 추가

--- week4/1.next.js/nextjs-routing/app/products/[productId]/reviews/[reviewId]/page.js
+++ week4/1.next.js/nextjs-routing/app/products/[productId]/reviews/[reviewId]/page.js
@@ -1,5 +1,5 @@
-export default function ProductDetail ({params}){
-    const { id } = await params
+export default async function ProductDetail(props) {
+    const params = await props.params;
     return (
         <div>상품 {params.productId}번  상세 리뷰 {params.reviewId}페이지</div>
     );
