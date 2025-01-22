52e4dce02a92a27b10d0905cf005d5d604a43358
sengjun0624 <sengjun0624@naver.com>
2025-01-22 22:08:58 +0900

    [CHAPTER15] Authentication

--- week4/nextjs-dashboard/app/lib/actions.ts
+++ week4/nextjs-dashboard/app/lib/actions.ts
@@ -1,5 +1,6 @@
 'use server';
-
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 import {z} from 'zod';
 import {sql} from '@vercel/postgres';
 import {revalidatePath} from "next/cache";
@@ -29,7 +30,24 @@ const FormSchema = z.object({
 const UpdateInvoice = FormSchema.omit({id: true, date: true});
 
 const CreateInvoice = FormSchema.omit({id: true, date: true});
-
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
+}
 export async function createInvoice(prevState: State, formData: FormData) {
     // Validate form using Zod
     const validatedFields = CreateInvoice.safeParse({
--- week4/nextjs-dashboard/app/ui/invoices/edit-form.tsx
+++ week4/nextjs-dashboard/app/ui/invoices/edit-form.tsx
@@ -9,7 +9,8 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import {Button} from '@/app/ui/button';
-import {updateInvoice} from '@/app/lib/actions';
+import {State, updateInvoice} from '@/app/lib/actions';
+import {useActionState} from "react";
 
 export default function EditInvoiceForm({
                                             invoice,
