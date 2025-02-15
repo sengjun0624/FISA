4920e923df049c44f5a33375583e16806c3b8a03
sengjun0624 <sengjun0624@naver.com>
2025-02-15 18:48:21 +0900

    add Functional Intergace

---
+++ week6/custom/src/main/java/dev/service/cloud/AddFunction.java
@@ -0,0 +1,7 @@
+package dev.service.cloud;
+
+@FunctionalInterface
+interface AddFunction {
+	int add(int a, int b);
+}
+
--- week6/custom/src/main/java/dev/service/cloud/Main.java
+++ week6/custom/src/main/java/dev/service/cloud/Main.java
@@ -4,6 +4,10 @@ import static util.AsciiArt.*;
 
 public class Main {
 	public static void main(String[] args) {
-		printGMG();
+		// 람다 표현식 사용
+		AddFunction addition = (a, b) -> a + b;
+
+		// 결과 출력
+		System.out.println("10 + 5 = " + addition.add(10, 5));
 	}
 }
