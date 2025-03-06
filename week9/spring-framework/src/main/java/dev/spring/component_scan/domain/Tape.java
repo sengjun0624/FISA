815789d9cccf89ce4465c27f6126656d4528f4de
sengjun0624 <sengjun0624@naver.com>
2025-03-06 12:14:35 +0900

    [WEEK9] Component Scan

---
+++ week9/spring-framework/src/main/java/dev/spring/component_scan/domain/Tape.java
@@ -0,0 +1,27 @@
+package dev.spring.component_scan.domain;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Tape {
+
+	private String name;
+	private boolean isWorked;
+
+	// @Autowired
+	public Tape(@Value("아일랜드") String name, @Value("true") boolean isWorked) {
+		super();
+		this.name = name;
+		this.isWorked = isWorked;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public boolean isWorked() {
+		return isWorked;
+	}
+
+}
