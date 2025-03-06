6c8c963e723671590854c24f7d0384fbac949698
sengjun0624 <sengjun0624@naver.com>
2025-03-06 11:15:35 +0900

    [WEEK9] Annotation Constructor  DI

--- week9/spring-framework/src/main/java/dev/spring/annotation/domain/TapeReader.java
+++ week9/spring-framework/src/main/java/dev/spring/annotation/domain/TapeReader.java
@@ -5,14 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class TapeReader {
 
 	private Tape tape;
-
 	@Autowired
-	public void setTape(Tape tape) {
+	public TapeReader(Tape tape) {
 		this.tape = tape;
 	}
 
-	public TapeReader(){
-	}
 	public void test() {
 		if (tape.isWorked()) {
 			System.out.println(tape.getName() + " 잘 동작합니다");
