93df4debc9ecdd90ffe418c4a63c57fbea7a13ac
sengjun0624 <sengjun0624@naver.com>
2025-03-11 09:46:20 +0900

    [WEEK10] chore

--- week9/step02_mvc_basic/src/main/java/dev/mvc/step02/mvc_basic/HomeController.java
+++ week9/step02_mvc_basic/src/main/java/dev/mvc/step02/mvc_basic/HomeController.java
@@ -6,12 +6,13 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
+@Component(value = "/mice")
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
