ac6e75fac8d868262a481346bc5cd0c5fafa90f7
sengjun0624 <sengjun0624@naver.com>
2025-03-17 12:18:05 +0900

    [WEEK11] Spring Security가 뭔지만 써보기

---
+++ week11/step01_hello_security/src/main/java/dev/security/step01_hello_security/HelloController.java
@@ -0,0 +1,15 @@
+package dev.security.step01_hello_security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/hello")
+	public void sayHello(){
+		System.out.println("Hello");
+
+	}
+
+}
---
+++ week11/step01_hello_security/src/test/java/dev/security/step01_hello_security/Step01HelloSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.security.step01_hello_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Step01HelloSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
