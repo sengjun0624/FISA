887c9dee3420dda20a54bd89cbc3e92635085c7e
sengjun0624 <sengjun0624@naver.com>
2025-04-09 10:08:26 +0900

    :sparkles [WEEK14] Rabbit Mq Publisher

--- week14/rabbit-mq/hello_rabbitmq_java/src/main/java/dev/mq/steo01/Receiver.java
+++ week14/rabbit-mq/hello_rabbitmq_java/src/main/java/dev/mq/steo01/Receiver.java
@@ -1,5 +1,9 @@
 package dev.mq.steo01;
 
+/**
+ * Receiver - 메시지 소비자 (Consumer)행
+ *
+ */
 public class Receiver {
 
 }
--- week14/rabbit-mq/hello_rabbitmq_java/src/main/java/dev/mq/steo01/Sender.java
+++ week14/rabbit-mq/hello_rabbitmq_java/src/main/java/dev/mq/steo01/Sender.java
@@ -1,4 +1,39 @@
 package dev.mq.steo01;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+/**
+ * Sender - 메시지 발행자 (Publisher)
+ *
+ * 퍼블리셔는 RabbitMq와 연결, 메시지를 전송하는 처리 수행
+ * 컨슈머는 메시지 큐로부터 퍼블리셔가 적재한 메시지를 소비하는 처리 수행
+ */
 public class Sender {
+	private static final String QUEUE_NAME = "hello";
+
+	public static void main(String[] args) throws IOException, TimeoutException {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+
+		// RabbitMQ 서버(5672)와 커넥션 연결 수행
+		Connection connection = factory.newConnection();
+		// 채녈 생성
+		Channel channel = connection.createChannel();
+
+		// 큐 생성
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+		String messgae = "Hell World!";
+
+		// Publish
+		channel
+			.basicPublish("", QUEUE_NAME, null, messgae.getBytes(StandardCharsets.UTF_8));
+		System.out.println(" [Publisher] Sent = " + messgae);
+	}
 }
