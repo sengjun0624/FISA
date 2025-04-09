1cc1766d89537d576fbd2cd07ead527bc15e86b1
sengjun0624 <sengjun0624@naver.com>
2025-04-09 15:27:06 +0900

    :sparkles [WEEK14] AMQP 스프링 init

---
+++ week14/rabbit-mq/SpringAPMQ/src/main/java/dev/apmq/springapmq/SpringApmqApplication.java
@@ -0,0 +1,13 @@
+package dev.apmq.springapmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApmqApplication.class, args);
+	}
+
+}
---
+++ week14/rabbit-mq/SpringAPMQ/src/test/java/dev/apmq/springapmq/SpringApmqApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.apmq.springapmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
