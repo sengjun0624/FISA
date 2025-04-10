b4d1b4e7d47c65fc98eb1f928ebf8e2554b7315f
sengjun0624 <sengjun0624@naver.com>
2025-04-10 09:47:34 +0900

    :sparkles: [WEEk14] Project Init

---
+++ week14/mvc-webflux/api-client/src/test/java/dev/client/ApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
