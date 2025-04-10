6918ebafdc0c2d475a78e53c2f2e2c85f8925aaa
sengjun0624 <sengjun0624@naver.com>
2025-04-10 10:21:17 +0900

    :sparkles: [WEEk14] Project Init

--- week14/mvc-webflux/api-client/src/test/java/dev/client/ApiClientApplicationTests.java
+++ week14/mvc-webflux/api-client/src/test/java/dev/client/ApiClientApplicationTests.java
@@ -1,13 +1,40 @@
 package dev.client;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.time.Instant;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootTest
 class ApiClientApplicationTests {
+	private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+	private static final String MVC_BASE_URL = "http://localhost:8080/mvc/delivery-info/";
+
+
+	private final HttpClient httpClient
 
 	@Test
-	void contextLoads() {
+	void MVC_요청() throws Exception {
+		Instant now = Instant.now();
+
+		for (int orderId : ORDER_IDS) {
+			HttpRequest.newBuilder()
+				.uri(new URI(MVC_BASE_URL + orderId))
+				.GET()
+				.build();
+		}
+		Instant end = Instant.now();
+		log.info("[동기 요청 종료] 총 소요 시간: {}ms ", Duration
+
 	}
 
 }
---
+++ week14/mvc-webflux/commerce-webflux/src/main/java/dev/webflux/CommerceWebfluxApplication.java
@@ -0,0 +1,13 @@
+package dev.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceWebfluxApplication.class, args);
+	}
+
+}
